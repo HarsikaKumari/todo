31c834695edfc88cc6265f2b04f40239ef5a0d4e
HarsikaKumari <kumariharsika8@gmail.com>
2023-12-24 22:55:33 +0530

    improvement

--- index.js
+++ index.js
@@ -1,36 +1,72 @@
+// Get the taskList
+const taskList = document.getElementById("taskList");
 // Get the add button
 const add = document.getElementById("add");
 // Add an onClick event listener
 add.addEventListener("click", addTask);
 
+// Create an array to store tasks
+let taskArray = [];
+// Call the function to show all the tasks
+showTask();
+
+// Create a function to show already added tasks
+function showTask() {
+  // Get all the tasks from local storage
+  const allTasks = JSON.parse(localStorage.getItem("com.tma.tasks")).tasks;
+
+  // Map the tasks and add it to taskList
+  allTasks.map(function (task) {
+    const li = generateLi(task.title, task.id);
+    taskList.appendChild(li);
+    taskArray.push(task);
+  });
+}
+
 // Create a function to add task to the list
 function addTask() {
   // Get the input
   const input = document.getElementById("taskInput");
-  // Get the taskList
-  const taskList = document.getElementById("taskList");
 
+  // Create current time for id
+  const id = new Date().getTime();
   // Create the li tag for the task items
-  const li = generateLi(input.value);
+  const li = generateLi(input.value, id);
   // Add the task to the ul
   taskList.appendChild(li);
+
+  // Create an task object
+  const task = {
+    id: id,
+    title: input.value,
+  };
+
+  // Add item to the taskArray
+  taskArray.push(task);
   // Clear the input value
   input.value = "";
+
+  // Save the task to local storage
+  localStorage.setItem("com.tma.tasks", JSON.stringify({ tasks: taskArray }));
 }
 
 // Create a function to generate html
-function generateLi(value) {
+function generateLi(value, id) {
   // Create li element and add class
   const li = document.createElement("li");
   li.className = "taskItem";
+  li.id = id;
 
+  // Create checkbox, p and dlt button
   const checkbox = generateCheckbox();
   const p = generateP(value);
-  const dltBtn = generateDltBtn(li);
+  const dltBtn = generateDltBtn();
+
+  // Create a div and add checkbox and p in it
+  const div = generateDiv(checkbox, p);
 
   // Add all the elements to the li
-  li.appendChild(checkbox);
-  li.appendChild(p);
+  li.appendChild(div);
   li.appendChild(dltBtn);
 
   // Return li
@@ -66,16 +102,43 @@ function generateP(value) {
   return p;
 }
 
+// Create a function to generate div tag
+function generateDiv(checkbox, p) {
+  // Create a div and add checkbox and p in it
+  const div = document.createElement("div");
+  div.className = "itemDetail";
+  div.appendChild(checkbox);
+  div.appendChild(p);
+
+  return div;
+}
+
 // Create a function to generate li
-function generateDltBtn(li) {
-  // Create dlt button and its required attribute
+function generateDltBtn() {
+  // Create delete button and its required attribute
   const dltBtn = document.createElement("button");
   dltBtn.type = "submit";
   dltBtn.className = "dlt";
   dltBtn.innerHTML = "Delete";
-  dltBtn.addEventListener("click", function () {
-    taskList.removeChild(li);
-  });
+
+  // Add an event listener to delete task on click
+  dltBtn.addEventListener("click", dltTask);
 
   return dltBtn;
+}
+
+// Create a function to delete task
+function dltTask(event) {
+  // Get the li
+  const li = event.target.parentElement;
+  // Remove the li from the taskList
+  taskList.removeChild(li);
+
+  // Remove the task from the array
+  taskArray = taskArray.filter(function (item) {
+    return item.id != li.id;
+  });
+
+  // Update the array in local storage
+  localStorage.setItem("com.tma.tasks", JSON.stringify({ tasks: taskArray }));
 }
